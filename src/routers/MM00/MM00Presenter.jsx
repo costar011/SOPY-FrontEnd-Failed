dc50d139e0023e31d2dfeb712569b314201ca9aa
YERIM <dnaer456@gmail.com>
2020-12-03 15:33:38 +0900

    MM00 Presenter

--- src/routers/MM00/MM00Presenter.jsx
+++ src/routers/MM00/MM00Presenter.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import styled from "styled-components";
+import { Wrapper, ImageBox } from "../../components/commonComponent";
+
+const Text = styled.div`
+  font-size: ${(props) => props.size};
+  font-weight: ${(props) => props.weight};
+  color: ${(props) => (props.isTitle ? props.theme.blackColor : ``)};
+  color: ${(props) => (props.isAuthor ? props.theme.whiteColor : ``)};
+  color: ${(props) => (props.isDesc ? props.theme.greyColor : ``)};
+  margin: 10px;
+`;
 
 const MM00Presenter = () => {
-  return `MM00`;
+  return <Wrapper></Wrapper>;
 };
 
 export default MM00Presenter;

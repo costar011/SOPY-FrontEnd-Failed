4d0c69c56e64d9cf8c9959887ac6d1741d2aa4bf
YERIM <dnaer456@gmail.com>
2020-12-03 15:11:29 +0900

    Theme

---
+++ src/routers/MM00/MM00Presenter.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+const MM00Presenter = () => {
+  return `MM00`;
+};
+
+export default MM00Presenter;
---
+++ src/styles/Theme.js
@@ -0,0 +1,7 @@
+export default {
+  mainThemeColor: "#0b0b0b",
+  subThemeColor: "#535c68",
+  pointColor: "#1e90ff",
+  errorColor: "#ff3838",
+  checkColor: "",
+};

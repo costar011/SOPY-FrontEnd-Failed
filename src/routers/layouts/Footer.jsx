b7eb84fdc651f02f0f26a80969173944685d1e40
YERIM <dnaer456@gmail.com>
2020-12-04 21:11:51 +0900

    Common

--- src/routers/layouts/Footer.jsx
+++ src/routers/layouts/Footer.jsx
@@ -1,6 +1,30 @@
 import React from "react";
 import styled from "styled-components";
 
+const FooterArea = styled.div`
+  width: 100%;
+  height: 500px;
+  background-color: ${(props) => props.theme.blackColor};
+  color: ${(props) => props.theme.whiteColor};
+`;
+
+const FooterAreaTop = styled.div`
+  width: 100%;
+  height: 50px;
+  background-color: ${(props) => props.theme.greyColor};
+  color: ${(props) => props.theme.whiteColor};
+`;
+
+const FooterAreaContent = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: ${(props) => props.theme.blackColor};
+  color: ${(props) => props.theme.whiteColor};
+  display: flex;
+  flex-direction: row;
+  padding: 25px;
+`;
+
 const Footer = () => {
   return `Footer`;
 };

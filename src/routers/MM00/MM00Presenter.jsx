0977e3121a803e9641728a04aa0d303e8fcaf9af
YERIM <dnaer456@gmail.com>
2020-12-09 19:17:49 +0900

    MM00

--- src/routers/MM00/MM00Presenter.jsx
+++ src/routers/MM00/MM00Presenter.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import styled from "styled-components";
 import { Wrapper, ImageBox } from "../../components/commonComponent";
 
-const Text = styled.div`
+const ViewText = styled.div`
+  margin: 6px;
   font-size: ${(props) => props.size};
   font-weight: ${(props) => props.weight};
   color: ${(props) => (props.isTitle ? props.theme.blackColor : ``)};
   color: ${(props) => (props.isAuthor ? props.theme.whiteColor : ``)};
   color: ${(props) => (props.isDesc ? props.theme.greyColor : ``)};
-  margin: 10px;
 `;
 
 const BarWrapper = styled.div`
   width: 1350px;
-  height: 50px;
-  border-radius: 10px;
+  height: 45px;
+  border-radius: 8px;
   background-color: ${(props) => props.theme.subThemeColor};
   box-shadow: ${(props) => props.theme.boxShadow};
   margin: 30px 0px;
@@ -22,7 +22,7 @@ const BarWrapper = styled.div`
 
 const WrapWrapper = styled(Wrapper)`
   width: 1350px;
-  flex-wrap: wrap; // width가 꽉 차면 자동으로 내려주는 녀석이다.
+  flex-wrap: wrap; // width가 꽉 차면 자동으로 내려주는 애
 `;
 
 const MM00Presenter = () => {
@@ -30,8 +30,18 @@ const MM00Presenter = () => {
     <Wrapper>
       <Wrapper width={`500px`} height={`30px`} dr={`row`}>
         <Wrapper al={`felx-end`} padding={`25px`}>
-          <ImageBox width={`400px`} height={`230px`}></ImageBox>
+          <ImageBox
+            width={`400px`}
+            height={`230px`}
+            bgImg={`https://images.unsplash.com/photo-1480506132288-68f7705954bd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1693&q=80`}
+          ></ImageBox>
         </Wrapper>
+      </Wrapper>
+      <Wrapper al={`flex-start`} padding={`20px`}>
+        wrapper
+        <ViewText size={`13.5px`} weight={`500`} isTitle={true}>
+          view
+        </ViewText>
       </Wrapper>
     </Wrapper>
   );

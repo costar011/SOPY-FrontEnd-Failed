3b76101137eab3c7558b59686dc571ec0aba5835
YERIM <dnaer456@gmail.com>
2021-01-03 19:41:05 +0900

    error

--- src/routers/MM00/MM00Container.jsx
+++ src/routers/MM00/MM00Container.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { useQuery } from "react-apollo-hooks";
 import MM00Presenter from "./MM00Presenter";
+import { GET_ALL_VIDEOS } from "./MM00Querise";
 
 const MM00Container = () => {
   ////////   VARIABLE     ////////
@@ -11,11 +13,19 @@ const MM00Container = () => {
   ////////   USE CONTEXT  ////////
 
   ////////   USE QUREY    ////////
+  /// { data, loading, refetch }
+  const {
+    data: videoDatum,
+    loading: videoLoding,
+    refetch: videoRefetch,
+  } = useQuery(GET_ALL_VIDEOS);
+
+  console.log(videoDatum && videoDatum.getAllVideos);
 
   ////////   USE MUTATION ////////
 
   ////////   USE EFFECT   ////////
 
-  return <MM00Presenter />;
+  return <MM00Presenter videoDatum={videoDatum && videoDatum.getAllvideos} />;
 };
 export default MM00Container;

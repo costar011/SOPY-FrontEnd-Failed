7894e8ec2644b98f6e997cab3c7d15acbcdcfaa1
YERIM <dnaer456@gmail.com>
2021-01-13 17:35:42 +0900

    MM05 styled

--- src/routers/MM05/MM05Presenter.jsx
+++ src/routers/MM05/MM05Presenter.jsx
@@ -25,6 +25,43 @@ const Title = styled.h2`
   margin: 0px 0px 40px 0px;
 `;
 
+const TextInput = styled.input`
+  width: ${(props) => props.width || `450px`};
+  height: 35px;
+  border-radius: 10px;
+  margin: 10px 0px;
+  padding: 0px 10px;
+  outline: none;
+  border: 1px solid ${(props) => props.theme.greyColor};
+  background: none;
+  box-shadow: ${(props) => props.theme.boxShadow};
+  transition: 0.5s;
+  &:hover {
+    box-shadow: 5px 5px 5px #0b0b0b;
+  }
+  &:focus {
+    box-shadow: 5px 5px 5px #0b0b0b;
+  }
+`;
+
+const Button = styled.button`
+  width: 250px;
+  height: 30px;
+  border-radius: ${(props) => props.theme.radius};
+  outline: none;
+  border: none;
+  background-color: ${(props) => props.theme.checkColor};
+  color: ${(props) => props.theme.whiteColor};
+  cursor: pointer;
+  margin: 20px 0px 50px 0px;
+  transition: 0.5s;
+  &:hover {
+    background-color: ${(props) => props.theme.whiteColor};
+    border: 1px solid ${(props) => props.theme.checkColor};
+    color: ${(props) => props.theme.checkColor};
+  }
+`;
+
 const MM05Presenter = () => {
   return (
     <Wrapper>
